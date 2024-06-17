8232f5bad142a92fafe3e8bcdecfb5eaab180277
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-06-17 16:37:12 +0330

    fix: remove the pwa package

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-import withPWA from "@ducanh2912/next-pwa";
-
-withPWA({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
