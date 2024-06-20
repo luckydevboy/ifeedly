061f8c9948be77f02c4803fa839ae8ee6371f8ec
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-06-20 20:23:53 +0330

    feat: handle virtualization with dynamic size

--- app/(with-layout)/page.tsx
+++ app/(with-layout)/page.tsx
@@ -23,15 +23,17 @@ export default function Home() {
 
   const parentRef = useRef(null);
 
-  const rowVirtualizer = useVirtualizer({
+  const virtualizer = useVirtualizer({
     count: hasNextPage ? allRows.length + 1 : allRows.length,
     getScrollElement: () => parentRef.current,
-    estimateSize: () => 176,
+    estimateSize: () => 45,
     overscan: 5,
   });
 
+  const items = virtualizer.getVirtualItems();
+
   useEffect(() => {
-    const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse();
+    const [lastItem] = [...items].reverse();
 
     if (!lastItem) {
       return;
@@ -44,13 +46,7 @@ export default function Home() {
     ) {
       fetchNextPage();
     }
-  }, [
-    hasNextPage,
-    fetchNextPage,
-    allRows.length,
-    isFetchingNextPage,
-    rowVirtualizer.getVirtualItems(),
-  ]);
+  }, [hasNextPage, fetchNextPage, allRows.length, isFetchingNextPage, items]);
 
   const addNewPost = (content: string) => {
     createPost.mutateAsync(content).then(() => {
@@ -67,57 +63,62 @@ export default function Home() {
         placeholder="Share your knowledge..."
       />
 
-      {!data && isFetching && (
+      {isFetching && !data && (
         <div className="text-center">
           <BeatLoader size={10} color="#7090E8" className="my-4" />
         </div>
       )}
 
       <div ref={parentRef} className={styles.parent}>
-        {/* The large inner element to hold all the items */}
         <div
           style={{
-            height: `${rowVirtualizer.getTotalSize()}px`,
+            height: virtualizer.getTotalSize(),
             width: "100%",
             position: "relative",
           }}
         >
-          {/* Only the visible items in the virtualizer, manually positioned to be in view */}
-          {rowVirtualizer.getVirtualItems().map((virtualItem) => {
-            const isLoaderRow = virtualItem.index > allRows.length - 1;
-            const post = allRows[virtualItem.index];
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              transform: `translateY(${items[0]?.start ?? 0}px)`,
+            }}
+          >
+            {items.map((virtualItem) => {
+              const isLoaderRow = virtualItem.index > allRows.length - 1;
+              const post = allRows[virtualItem.index];
+              const isLastRow = virtualItem.index === allRows.length - 1;
 
-            return (
-              <div
-                key={virtualItem.key}
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: `${virtualItem.size}px`,
-                  transform: `translateY(${virtualItem.start}px)`,
-                }}
-              >
-                {isLoaderRow ? (
-                  hasNextPage ? (
-                    <div className="text-center">
-                      <BeatLoader size={10} color="#7090E8" className="my-4" />
-                    </div>
+              return (
+                <div
+                  key={virtualItem.key}
+                  data-index={virtualItem.index}
+                  ref={virtualizer.measureElement}
+                >
+                  {isLoaderRow ? (
+                    hasNextPage ? (
+                      <div className="text-center">
+                        <BeatLoader
+                          size={10}
+                          color="#7090E8"
+                          className="my-4"
+                        />
+                      </div>
+                    ) : (
+                      "Nothing more to load"
+                    )
                   ) : (
-                    "Nothing more to load"
-                  )
-                ) : (
-                  <>
-                    <Card {...post} type="post" />
-                    {virtualItem.index !== allRows.length - 1 && (
-                      <hr className="absolute w-full bottom-5 border-zinc-100" />
-                    )}
-                  </>
-                )}
-              </div>
-            );
-          })}
+                    <>
+                      <Card {...post} type="post" />
+                      {!isLastRow && <hr className="my-6 border-zinc-100" />}
+                    </>
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </main>
